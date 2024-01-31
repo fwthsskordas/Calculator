dccf70d4f735287efcb037e4de26dd87c701c57b
fotis <fotis@arx.net>
2024-01-31 16:38:27 +0200

    Fix: operator functionality

--- script.js
+++ script.js
@@ -2,16 +2,40 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === "0" ? number : displayValue + number;
+  //   const displayValue = calculatorDisplay.textContent;
+  //   calculatorDisplay.textContent =
+  //   displayValue === "0" ? number : displayValue + number;
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
+  if (awaitingNextValue) return;
   if (!calculatorDisplay.textContent.includes(".")) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function userOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    // currentValue =
+  }
+  awaitingNextValue = true;
+  operatorValue = operator;
 }
 
 //events
@@ -19,13 +43,16 @@ inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("operator")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => userOperator(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
     inputBtn.addEventListener("click", () => addDecimal());
   }
 });
 
 function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
   calculatorDisplay.textContent = "0";
 }
 
