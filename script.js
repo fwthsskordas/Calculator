395ec8c16c19465ea18387c9a4caf3fcffffe8e9
fotis <fotis@arx.net>
2024-01-31 16:07:52 +0200

    Display now can show numbers and operators

---
+++ script.js
@@ -0,0 +1,32 @@
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+
+function sendNumberValue(number) {
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
+}
+
+function addDecimal() {
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
+//events
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => addDecimal());
+  }
+});
+
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+clearBtn.addEventListener("click", resetAll);
